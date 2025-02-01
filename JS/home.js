361500bbc67302aed370f56eaf85da8bc36a6661
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-02-01 23:18:26 +0600

    Balance Updated

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,20 @@
-console.log('JS file connected')
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addAmount = document.getElementById('input-amount').value;
+
+    const pin = document.getElementById('input-pin').value;
+    
+    // pin validate
+    if(pin === '1234'){
+        // Add balance
+        const accountBalance = document.getElementById('account-balance').innerText;
+        const newBalance = parseFloat(accountBalance) + parseFloat(addAmount);
+
+        // Update Balance
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Incorrect Pin');
+    }
+})
